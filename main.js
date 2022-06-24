a5382d5773e199baf0122f1b5d937d0b6a09a89f
NegativeSector <67027632+dfghjkjhgr@users.noreply.github.com>
2022-06-24 17:00:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const $ = document.querySelector.bind(document);
+$('#big-button').addEventListener('click', function() {
+  let sign = $('#sign');
+	switch (getRandomInt(3)) {
+		case 2:
+			if (sign.innerText !== 'Yay!'){
+				sign.innerText = 'Yay!';
+			} else {
+				sign.innerText = 'Woohoo!';
+			}
+		case 1:
+			setInterval(function() {
+				$('body').style.backgroundColor = `hsl(${getRandomInt(256)}, 90%, 90%)`
+			}, 1000);
+		case 0:
+			document.title = 'Yay!!!!!!';
+	}
+	
+})
